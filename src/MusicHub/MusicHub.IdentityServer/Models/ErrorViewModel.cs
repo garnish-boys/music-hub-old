372fd5f77765dfee9bcfa026640cb3c3a7b49bba
Tyler Simmons <tyler.simmons1996@gmail.com>
2021-12-02 19:43:18 -0500

    Add project files

---
+++ src/MusicHub/MusicHub.IdentityServer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MusicHub.IdentityServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
